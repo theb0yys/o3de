d4e43e636fbe35e7991e7177ca7e1513ffebef9e
theb0yys <kanejames103@gmail.com>
2026-07-17 07:27:34 +0100

    Add Tainted Grail SDK system component

---
+++ Gems/TaintedGrailModdingSDK/Code/Source/TaintedGrailModdingSDKSystemComponent.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ */
+
+#pragma once
+
+#include <AzCore/Component/Component.h>
+
+namespace TaintedGrailModdingSDK
+{
+    class TaintedGrailModdingSDKSystemComponent final
+        : public AZ::Component
+    {
+    public:
+        AZ_COMPONENT(
+            TaintedGrailModdingSDKSystemComponent,
+            "{CCAB1D0B-9DDC-4313-906F-47B7148109AC}");
+
+        static void Reflect(AZ::ReflectContext* context);
+        static void GetProvidedServices(AZ::ComponentDescriptor::DependencyArrayType& provided);
+        static void GetIncompatibleServices(AZ::ComponentDescriptor::DependencyArrayType& incompatible);
+
+        void Activate() override;
+        void Deactivate() override;
+    };
+} // namespace TaintedGrailModdingSDK
