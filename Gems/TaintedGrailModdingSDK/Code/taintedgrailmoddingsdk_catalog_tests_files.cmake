e30d1c68c47dfeadd27fdd5eb4104907311e1632
theb0yys <kanejames103@gmail.com>
2026-07-17 14:15:09 +0100

    Register canonical catalog test sources

---
+++ Gems/TaintedGrailModdingSDK/Code/taintedgrailmoddingsdk_catalog_tests_files.cmake
@@ -0,0 +1,14 @@
+#
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+
+set(FILES
+    Source/CatalogDatabase.cpp
+    Source/CatalogDatabase.h
+    Source/FoundationModels.cpp
+    Source/FoundationModels.h
+    Tests/CatalogDatabaseTests.cpp
+)
