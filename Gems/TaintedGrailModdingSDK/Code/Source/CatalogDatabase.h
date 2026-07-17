7e45d1493101549f14b274a248aa1097a230fc52
theb0yys <kanejames103@gmail.com>
2026-07-17 16:30:41 +0100

    Extend canonical catalog for economy profiles

--- Gems/TaintedGrailModdingSDK/Code/Source/CatalogDatabase.h
+++ Gems/TaintedGrailModdingSDK/Code/Source/CatalogDatabase.h
@@ -21,6 +21,10 @@ namespace TaintedGrailModdingSDK
         bool UpsertRelationship(const CatalogRelationship& relationship, AZStd::string* error = nullptr);
         bool AddValidationEvent(const CatalogValidationEvent& validation, AZStd::string* error = nullptr);
         bool AddGovernanceEvent(const CatalogGovernanceEvent& event, AZStd::string* error = nullptr);
+        bool UpsertEconomyItem(const EconomyItemProfile& profile, AZStd::string* error = nullptr);
+        bool UpsertEconomyRecipe(const EconomyRecipeProfile& profile, AZStd::string* error = nullptr);
+        bool UpsertRecipeIngredient(const EconomyRecipeIngredient& ingredient, AZStd::string* error = nullptr);
+        bool UpsertRecipeOutput(const EconomyRecipeOutput& output, AZStd::string* error = nullptr);
         bool ReplaceFromDocument(const CatalogDocument& document, AZStd::string* error = nullptr);
         void Clear();
 
@@ -30,6 +34,8 @@ namespace TaintedGrailModdingSDK
         const CatalogRecord* FindByExactNativeRef(const AZStd::string& nativeRefExact) const;
         const CatalogRelationship* FindRelationshipById(const AZStd::string& relationshipId) const;
         const CatalogValidationEvent* FindValidationById(const AZStd::string& validationId) const;
+        const EconomyItemProfile* FindEconomyItem(const AZStd::string& recordId) const;
+        const EconomyRecipeProfile* FindEconomyRecipe(const AZStd::string& recordId) const;
 
         AZStd::vector<CatalogRecord> Query(const CatalogQuery& query) const;
         AZStd::vector<CatalogRelationship> FindRelationshipsForRecord(const AZStd::string& recordId) const;
@@ -40,6 +46,8 @@ namespace TaintedGrailModdingSDK
         AZStd::vector<CatalogGovernanceEvent> FindGovernanceForSubject(
             const AZStd::string& subjectKind,
             const AZStd::string& subjectId) const;
+        AZStd::vector<EconomyRecipeIngredient> FindIngredientsForRecipe(const AZStd::string& recipeRecordId) const;
+        AZStd::vector<EconomyRecipeOutput> FindOutputsForRecipe(const AZStd::string& recipeRecordId) const;
         AZStd::vector<DomainCoverage> BuildCoverage() const;
 
         CatalogDocument BuildDocument(
@@ -50,15 +58,27 @@ namespace TaintedGrailModdingSDK
         const AZStd::vector<CatalogRelationship>& GetRelationships() const;
         const AZStd::vector<CatalogValidationEvent>& GetValidationHistory() const;
         const AZStd::vector<CatalogGovernanceEvent>& GetGovernanceHistory() const;
+        const AZStd::vector<EconomyItemProfile>& GetEconomyItems() const;
+        const AZStd::vector<EconomyRecipeProfile>& GetEconomyRecipes() const;
+        const AZStd::vector<EconomyRecipeIngredient>& GetRecipeIngredients() const;
+        const AZStd::vector<EconomyRecipeOutput>& GetRecipeOutputs() const;
 
     private:
         bool ValidateRecord(const CatalogRecord& record, const CatalogRecord* replacing, AZStd::string* error) const;
         bool ValidateRelationship(const CatalogRelationship& relationship, AZStd::string* error) const;
         bool ValidateGovernanceEvent(const CatalogGovernanceEvent& event, AZStd::string* error) const;
+        bool ValidateEconomyItem(const EconomyItemProfile& profile, AZStd::string* error) const;
+        bool ValidateEconomyRecipe(const EconomyRecipeProfile& profile, AZStd::string* error) const;
+        bool ValidateRecipeIngredient(const EconomyRecipeIngredient& ingredient, AZStd::string* error) const;
+        bool ValidateRecipeOutput(const EconomyRecipeOutput& output, AZStd::string* error) const;
 
         AZStd::vector<CatalogRecord> m_records;
         AZStd::vector<CatalogRelationship> m_relationships;
         AZStd::vector<CatalogValidationEvent> m_validationHistory;
         AZStd::vector<CatalogGovernanceEvent> m_governanceHistory;
+        AZStd::vector<EconomyItemProfile> m_economyItems;
+        AZStd::vector<EconomyRecipeProfile> m_economyRecipes;
+        AZStd::vector<EconomyRecipeIngredient> m_recipeIngredients;
+        AZStd::vector<EconomyRecipeOutput> m_recipeOutputs;
     };
 } // namespace TaintedGrailModdingSDK
