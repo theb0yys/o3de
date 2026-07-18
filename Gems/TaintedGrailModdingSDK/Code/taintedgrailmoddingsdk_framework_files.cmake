92bdefbcaad7489fbc755f1f0c4cd27f187ff5bf
theb0yys <kanejames103@gmail.com>
2026-07-18 20:18:58 +0100

    Add Tainted Framework source ownership
    Signed-off-by: theb0yys <kanejames103@gmail.com>

---
+++ Gems/TaintedGrailModdingSDK/Code/taintedgrailmoddingsdk_framework_files.cmake
@@ -0,0 +1,36 @@
+#
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+
+set(FILES
+    Source/CatalogPersistenceService.cpp
+    Source/CatalogPersistenceService.h
+    Source/FoundationCatalogService.cpp
+    Source/FoundationEconomyService.cpp
+    Source/FoundationGovernanceService.cpp
+    Source/FoundationNotificationBus.h
+    Source/FoundationPersistenceBoundary.cpp
+    Source/FoundationPersistenceBoundary.h
+    Source/FoundationService.cpp
+    Source/FoundationService.h
+    Source/FoundationServiceConstruction.cpp
+    Source/FoundationWorkspaceLoadService.cpp
+    Source/FoundationWorkspaceLoadService.h
+    Source/PackPersistenceService.cpp
+    Source/PackPersistenceService.h
+    Source/PathPolicyService.cpp
+    Source/PathPolicyService.h
+    Source/PathPolicyWorkspaceValidation.cpp
+    Source/PersistenceJsonUtils.h
+    Source/SourceEvidencePersistenceService.cpp
+    Source/SourceEvidencePersistenceService.h
+    Source/SourceImportService.cpp
+    Source/SourceImportService.h
+    Source/WorkspacePersistenceService.cpp
+    Source/WorkspacePersistenceService.h
+    Source/WorkspaceSchemaService.cpp
+    Source/WorkspaceSchemaService.h
+)
