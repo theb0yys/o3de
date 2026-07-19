011b28df5080e521c5279f7d2e75ce4993accf50
theb0yys <kanejames103@gmail.com>
2026-07-19 22:31:28 +0100

    Close catalog mutation and binding bypasses

--- Gems/TaintedGrailModdingSDK/Code/Source/CatalogDatabase.h
+++ Gems/TaintedGrailModdingSDK/Code/Source/CatalogDatabase.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include "FoundationModels.h"
+#include "SourceEvidenceRegistry.h"
 
 #include <AzCore/std/utility/move.h>
 
@@ -18,36 +19,75 @@ namespace TaintedGrailModdingSDK
     public:
         bool InsertNew(const CatalogRecord& record, AZStd::string* error = nullptr);
         bool Upsert(const CatalogRecord& record, AZStd::string* error = nullptr);
-        bool UpsertRelationship(const CatalogRelationship& relationship, AZStd::string* error = nullptr);
-        bool AddValidationEvent(const CatalogValidationEvent& validation, AZStd::string* error = nullptr);
-        bool AddGovernanceEvent(const CatalogGovernanceEvent& event, AZStd::string* error = nullptr);
-        bool UpsertEconomyItem(const EconomyItemProfile& profile, AZStd::string* error = nullptr);
-        bool UpsertEconomyRecipe(const EconomyRecipeProfile& profile, AZStd::string* error = nullptr);
-        bool UpsertRecipeIngredient(const EconomyRecipeIngredient& ingredient, AZStd::string* error = nullptr);
-        bool UpsertRecipeOutput(const EconomyRecipeOutput& output, AZStd::string* error = nullptr);
-        bool ReplaceFromDocument(const CatalogDocument& document, AZStd::string* error = nullptr);
+        bool UpsertRelationship(
+            const CatalogRelationship& relationship,
+            AZStd::string* error = nullptr);
+        bool AddValidationEvent(
+            const CatalogValidationEvent& validation,
+            AZStd::string* error = nullptr);
+        bool AddGovernanceEvent(
+            const CatalogGovernanceEvent& event,
+            AZStd::string* error = nullptr);
+        bool UpsertEconomyItem(
+            const EconomyItemProfile& profile,
+            AZStd::string* error = nullptr);
+        bool UpsertEconomyRecipe(
+            const EconomyRecipeProfile& profile,
+            AZStd::string* error = nullptr);
+        bool UpsertRecipeIngredient(
+            const EconomyRecipeIngredient& ingredient,
+            AZStd::string* error = nullptr);
+        bool UpsertRecipeOutput(
+            const EconomyRecipeOutput& output,
+            AZStd::string* error = nullptr);
+        bool ReplaceFromBoundDocument(
+            const CatalogDocument& document,
+            const WorkspaceModel& workspace,
+            const GameProfile& profile,
+            const SourceEvidenceRegistry& sourceRegistry,
+            AZStd::string* error = nullptr);
+        bool ValidateIntegrity(
+            const WorkspaceModel& workspace,
+            const GameProfile& profile,
+            const SourceEvidenceRegistry& sourceRegistry,
+            AZStd::string* error = nullptr) const;
         void Clear();
 
-        CatalogRecord* FindMutableRecordById(const AZStd::string& recordId);
-        CatalogRelationship* FindMutableRelationshipById(const AZStd::string& relationshipId);
         const CatalogRecord* FindByRecordId(const AZStd::string& recordId) const;
-        const CatalogRecord* FindByExactNativeRef(const AZStd::string& nativeRefExact) const;
-        const CatalogRelationship* FindRelationshipById(const AZStd::string& relationshipId) const;
-        const CatalogValidationEvent* FindValidationById(const AZStd::string& validationId) const;
-        const EconomyItemProfile* FindEconomyItem(const AZStd::string& recordId) const;
-        const EconomyRecipeProfile* FindEconomyRecipe(const AZStd::string& recordId) const;
+        const CatalogRecord* FindByExactNativeRef(
+            const AZStd::string& nativeRefExact) const;
+        const CatalogRelationship* FindRelationshipById(
+            const AZStd::string& relationshipId) const;
+        const CatalogValidationEvent* FindValidationById(
+            const AZStd::string& validationId) const;
+        const CatalogValidationEvent* FindLatestValidationForSubject(
+            const AZStd::string& subjectKind,
+            const AZStd::string& subjectId) const;
+        const CatalogGovernanceEvent* FindEffectiveGovernanceEvent(
+            const AZStd::string& subjectKind,
+            const AZStd::string& subjectId,
+            const AZStd::string& axis,
+            const AZStd::string& usage = {}) const;
+        const EconomyItemProfile* FindEconomyItem(
+            const AZStd::string& recordId) const;
+        const EconomyRecipeProfile* FindEconomyRecipe(
+            const AZStd::string& recordId) const;
 
         AZStd::vector<CatalogRecord> Query(const CatalogQuery& query) const;
-        AZStd::vector<CatalogRelationship> FindRelationshipsForRecord(const AZStd::string& recordId) const;
-        AZStd::vector<CatalogValidationEvent> FindValidationForRecord(const AZStd::string& recordId) const;
+        AZStd::vector<CatalogRelationship> FindRelationshipsForRecord(
+            const AZStd::string& recordId) const;
+        AZStd::vector<CatalogValidationEvent> FindValidationForRecord(
+            const AZStd::string& recordId) const;
         AZStd::vector<CatalogValidationEvent> FindValidationForSubject(
             const AZStd::string& subjectKind,
             const AZStd::string& subjectId) const;
         AZStd::vector<CatalogGovernanceEvent> FindGovernanceForSubject(
             const AZStd::string& subjectKind,
             const AZStd::string& subjectId) const;
-        AZStd::vector<EconomyRecipeIngredient> FindIngredientsForRecipe(const AZStd::string& recipeRecordId) const;
-        AZStd::vector<EconomyRecipeOutput> FindOutputsForRecipe(const AZStd::string& recipeRecordId) const;
+        AZStd::vector<EconomyRecipeIngredient> FindIngredientsForRecipe(
+            const AZStd::string& recipeRecordId) const;
+        AZStd::vector<EconomyRecipeOutput> FindOutputsForRecipe(
+            const AZStd::string& recipeRecordId) const;
         AZStd::vector<DomainCoverage> BuildCoverage() const;
 
         CatalogDocument BuildDocument(
@@ -64,13 +104,34 @@ namespace TaintedGrailModdingSDK
         const AZStd::vector<EconomyRecipeOutput>& GetRecipeOutputs() const;
 
     private:
-        bool ValidateRecord(const CatalogRecord& record, const CatalogRecord* replacing, AZStd::string* error) const;
-        bool ValidateRelationship(const CatalogRelationship& relationship, AZStd::string* error) const;
-        bool ValidateGovernanceEvent(const CatalogGovernanceEvent& event, AZStd::string* error) const;
-        bool ValidateEconomyItem(const EconomyItemProfile& profile, AZStd::string* error) const;
-        bool ValidateEconomyRecipe(const EconomyRecipeProfile& profile, AZStd::string* error) const;
-        bool ValidateRecipeIngredient(const EconomyRecipeIngredient& ingredient, AZStd::string* error) const;
-        bool ValidateRecipeOutput(const EconomyRecipeOutput& output, AZStd::string* error) const;
+        CatalogRecord* FindMutableRecordById(const AZStd::string& recordId);
+        CatalogRelationship* FindMutableRelationshipById(
+            const AZStd::string& relationshipId);
+        bool ReplaceFromDocumentUnchecked(
+            const CatalogDocument& document,
+            AZStd::string* error);
+        bool ValidateRecord(
+            const CatalogRecord& record,
+            const CatalogRecord* replacing,
+            AZStd::string* error) const;
+        bool ValidateRelationship(
+            const CatalogRelationship& relationship,
+            AZStd::string* error) const;
+        bool ValidateGovernanceEvent(
+            const CatalogGovernanceEvent& event,
+            AZStd::string* error) const;
+        bool ValidateEconomyItem(
+            const EconomyItemProfile& profile,
+            AZStd::string* error) const;
+        bool ValidateEconomyRecipe(
+            const EconomyRecipeProfile& profile,
+            AZStd::string* error) const;
+        bool ValidateRecipeIngredient(
+            const EconomyRecipeIngredient& ingredient,
+            AZStd::string* error) const;
+        bool ValidateRecipeOutput(
+            const EconomyRecipeOutput& output,
+            AZStd::string* error) const;
 
         AZStd::vector<CatalogRecord> m_records;
         AZStd::vector<CatalogRelationship> m_relationships;
