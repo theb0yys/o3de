015ea703983555de4a5ed8789e7ffe0547362184
theb0yys <kanejames103@gmail.com>
2026-07-22 19:40:40 +0100

    Add canonical interchange test source inventory
    Signed-off-by: theb0yys <theb0yys@users.noreply.github.com>

---
+++ Gems/TaintedGrailModdingSDK/Code/taintedgrailmoddingsdk_canonical_interchange_tests_files.cmake
@@ -0,0 +1,15 @@
+#
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+
+set(FILES
+    Tests/Main.cpp
+    Tests/CanonicalInterchangeTestFixtures.h
+    Tests/CanonicalInterchangeTypesTests.cpp
+    Tests/CanonicalInterchangeCanonicalTests.cpp
+    Tests/CanonicalInterchangeValidationTests.cpp
+    Tests/CanonicalInterchangeMigrationTests.cpp
+)
